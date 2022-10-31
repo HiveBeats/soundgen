fdc5c2f112f60b6fa08360571c184c9603eec1ae
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-10-31 18:42:36 +0400

    fix: saw and triangle waves

--- SoundGen/Oscillator.fs
+++ SoundGen/Oscillator.fs
@@ -1,33 +1,15 @@
 module SoundGen.Oscillator
-
+open System
 open Settings
 
+let pos hz x = (hz * x / sampleRate) % 1.
+
 let sineosc hz x =
-    x
-    |> (*) (2. * System.Math.PI * hz / sampleRate)
-    |> sin
+    x |> (*) (2. * Math.PI * hz / sampleRate) |> sin
 
 let squareosc hz x =
     let sign v : float = if v > 0.0 then 1. else -1.
     x |> sineosc hz |> sign
-
-let triangleosc hz x =
-    let fullPeriodTime = 1.0 / hz
-    let localTime = x % fullPeriodTime
-    let value = localTime / fullPeriodTime
-
-    if (value < 0.25) then
-        value * 4.
-    else if (value < 0.75) then
-        2.0 - (value * 4.0)
-    else
-        value * 4. - 4.0
-        
-let sawosc hz x =
-    let fullPeriodTime = 1.0 / hz;
-    let localTime = x % fullPeriodTime
-   
-    ((localTime / fullPeriodTime) * 2. - 1.0);
-
-let sinesquareosc hz x =
-    sineosc (hz/4.) x + squareosc hz x
+let triangleosc (hz:float) (x:float) = 1. - Math.Abs((pos hz x) - 0.5) * 4.
+let sawosc hz x = (pos hz x) * 2. - 1.
+let sinesquareosc hz x = sawosc (hz / 4.) x + squareosc (hz/2.) x
